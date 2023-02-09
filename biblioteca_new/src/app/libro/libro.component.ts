70d9f2d97f809442899265d3d897bc8a950e43df
Santiago Leal <santi.leal.c.678@gmail.com>
2023-02-08 19:26:01 -0500

    libr.service.ts preparado

---
+++ biblioteca_new/src/app/libro/libro.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-libro',
+  templateUrl: './libro.component.html',
+  styleUrls: ['./libro.component.css']
+})
+export class LibroComponent implements OnInit {
+
+  titulo = 'Cien años de soledad';
+
+  constructor(){
+
+  }
+  ngOnInit():void{
+
+  }
+
+}
